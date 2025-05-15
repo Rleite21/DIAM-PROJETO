e1758194a6236efce1fbe9fecddb7e14934f954a
Rleite21 <rrressleite@gmail.com>
2025-05-15 19:54:12 +0100

    Limpeza geral

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Header from '../Header/Header';
-import Calc_Finos from '../Calc_Finos/Calc_Finos';
-import Vol_Req from '../Vol_Req/Vol_Req';
-import Cards from '../Cards/Cards';
+import Calculadora from './Calculadora';
 import Mapa from './mapa'; 
 import Footer from './Footer';
 import ProximosEventos from './Eventos';
@@ -13,6 +11,7 @@ const Home = () => {
     <div className='app-container' style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
       <Header />
       <Mapa /> 
+      <Calculadora />
       <Footer />
       <ProximosEventos />
     </div>
